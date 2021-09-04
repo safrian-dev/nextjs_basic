66963ffd78c1d3354dab891c763c0060bf53cf8b
unknown <hello.safrian@gmail.com>
2021-09-04 14:30:01 +0700

    auto redirection

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,17 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react"
+
 export default function Custom4040() {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 2000)
+    }, [])
+    // set waktu untuk redirect
+    // [] digunakan untuk sekali pemanggilan saja
+
     return (
         <div>
             <h1 className="title-not-found">Ooops, halaman tidak ditemukan..</h1>
