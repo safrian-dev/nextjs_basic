425a1a6bc9cff021462af41b6e3dc5035db1ff1b
unknown <hello.safrian@gmail.com>
2021-09-04 11:55:03 +0700

    membuat global & module css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header"
+import Footer from "../components/Footer"
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
       <Header/>
+      <h1 className={styles['title-homepage']}>Welcome my bro</h1>
+      <Footer/>
     </div>
   )
 }
